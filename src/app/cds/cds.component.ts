7622f85421a4e5a71f280092e4c5d07d5eaca075
Marcus Ramirez <macusramirez@gmail.com>
2022-01-31 10:01:42 -0600

    added modal

--- src/app/cds/cds.component.ts
+++ src/app/cds/cds.component.ts
@@ -3,7 +3,7 @@ import { CdServiceService } from '../cd-service.service';
 import { Cd } from '../cd/cd.model';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import {coerceNumberProperty} from '@angular/cdk/coercion';
+import { coerceNumberProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-cds',
@@ -23,13 +23,30 @@ export class CdsComponent implements OnInit {
   interestRate: number = 2;
   calculatedInterest: any = 0;
   currentValue: number = 0;
-  calculatedReturn: number=0;
-  value = 0;  
+  calculatedReturn: number = 0;
+  value = 0;
+  openingDeposit: number = 0;
+
   ngOnInit(): void {
     this.cdService.getCds().subscribe((payload) => {
       this.cds = payload;
     });
   }
+
+  displayStyle = 'none';
+  
+  openPopup() {
+    this.displayStyle = "block";
+  }
+
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
+  buyCd(){
+    console.log("congrats")
+  }
+
 
   formatLabel(value: number) {
     if (value >= 1000) {
@@ -39,24 +56,33 @@ export class CdsComponent implements OnInit {
     return value;
   }
 
-  calculateReturn(minDeposit: any, termLength: any, interestRate: any, event:any) {
-    
+  calculateReturn(
+    minDeposit: any,
+    termLength: any,
+    interestRate: any,
+    event: any
+  ) {
     this.calculatedInterest =
-     minDeposit * Math.pow(1 + interestRate / 365, termLength);
+      minDeposit * Math.pow(1 + interestRate / 365, termLength);
     this.calculatedReturn = this.calculatedInterest.toFixed(2);
-     this.value = coerceNumberProperty(event)
-    
+    this.value = coerceNumberProperty(event);
   }
 
-  sliderCalc(event:any, termLength: any, interestRate: any) {
-this.calculatedReturn =
-event.value * Math.pow(1 + interestRate / 365, termLength);
+  sliderCalc(event: any, termLength: any, interestRate: any) {
+    this.calculatedReturn =
+      event.value * Math.pow(1 + interestRate / 365, termLength);
   }
 
   xferData(minDeposit: any, termLength: any, interestRate: any) {
     this.minDeposit = minDeposit;
     this.termLength = termLength;
     this.interestRate = interestRate;
-    this.calculateReturn(this.minDeposit, this.termLength, this.interestRate, event);
+    this.calculateReturn(
+      this.minDeposit,
+      this.termLength,
+      this.interestRate,
+      event
+    );
   }
+
 }
