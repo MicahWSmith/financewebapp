0886b0c1eee37aefa6ba0105816b9542908069a0
Dakota Korn <DakotaJKorn@gmail.com>
2022-02-02 16:45:38 -0500

    Changed timeout length and updated loggin/loggout functions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardCommunicationService } from '../dashboard/dashboard-communication.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   loggedIn: boolean = false;
-  constructor() { }
+  constructor(private dashboardCommunicationService: DashboardCommunicationService) { 
+    dashboardCommunicationService.setLandingPage(this);
+  }
 
   ngOnInit(): void {
     let session = sessionStorage.getItem('user') ? sessionStorage.getItem('user') : "";
@@ -16,6 +19,10 @@ export class HomeComponent implements OnInit {
     if(session != ""){
       this.loggedIn = true;
     }
+  }
+
+  setLoggedOff(){
+    this.loggedIn = false;
   }
 
 }
