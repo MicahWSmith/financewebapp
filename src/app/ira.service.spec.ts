90d0b5e9b7ffcf4e587eaed1b50cd1af41e1b9f3
Rae <RaeTStone@gmail.com>
2022-02-02 12:41:34 -0500

    updated all of my service/components to pass tests

--- src/app/ira.service.spec.ts
+++ src/app/ira.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IraService } from './ira.service';
 
 describe('IraService', () => {
   let service: IraService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[IraService]
+    });
     service = TestBed.inject(IraService);
   });
 
--- src/app/ira/ira.component.spec.ts
+++ src/app/ira/ira.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IraComponent } from './ira.component';
 
 describe('IraComponent', () => {
@@ -8,6 +8,7 @@ describe('IraComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
       declarations: [ IraComponent ]
     })
     .compileComponents();
