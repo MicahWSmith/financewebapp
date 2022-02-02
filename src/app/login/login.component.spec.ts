3d9d467ed486fe827a4e7836db412c328571eef9
Micah Smith <86262666+MicahWSmith@users.noreply.github.com>
2022-02-02 17:24:34 -0500

    Service and component testing fixes

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule, RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
